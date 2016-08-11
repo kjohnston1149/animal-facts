1f0392c8801cd4681fe22fed2adc6dfba112a897
Andrew Merrell and Kevin Johnston <andrew@>
2016-08-11 09:06:59 -0700

    Add branching for different animal groups.

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+$(function() {
+  $("form").submit(function(event){
+
+
+    var animal = $("#animalTypes").val();
+
+    if (animal === "Turtles"){
+      $('#turtles').show();
+      $('#snakes').hide();
+      $('#insects').hide();
+    } else if (animal === "Snakes") {
+      $('#snakes').show();
+      $('#turtles').hide();
+      $('#insects').hide();
+    } else { (animal === "Insects")
+        $('#insects').show();
+        $('#turtles').hide();
+        $('#snakes').hide();
+    }
+
+    event.preventDefault();
+  });
+
+});
